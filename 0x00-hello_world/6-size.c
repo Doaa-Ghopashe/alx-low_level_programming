0fd45e3448fbba556bd9b76a7a0822045b66ea66
Doaa-Ghopashe <doaaadel252@yahoo.com>
2023-02-19 13:28:56 -0800

    this file contains the command of sizeof to get the size of different datatypes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char word;
+int num;
+long int lnum;
+long long int llnum;
+float fnum;
+printf("Size of a char: %ld byte(s)\n" ,sizeof(word));
+printf("Size of an int: %ld byte(s)\n",sizeof(num));
+printf("Size of a long int: %ld byte(s)\n",sizeof(lnum));
+printf("Size of a long long int: %ld byte(s)\n",sizeof(llnum));
+printf("Size of a float: %ld byte(s)\n",sizeof(fnum));
+return (0);
+}
