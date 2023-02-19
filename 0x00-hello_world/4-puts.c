52b47f9402e979815edb5d49570209a9794e3691
Doaa-Ghopashe <doaaadel252@yahoo.com>
2023-02-19 12:19:04 -0800

    the first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-
-int main() {
-    char str []= "\"Programming is like building a multilingual puzzle";
-
-   puts(str);
-    return 0;
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void) {
+    char str[]= "\"Programming is like building a multilingual puzzle";
+    puts(str);
+    return (0);
 }
