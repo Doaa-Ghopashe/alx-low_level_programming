2b1ec42958d45b0510b4a60f1cc69b76d9a7e99d
Doaa-Ghopashe <doaaadel252@yahoo.com>
2023-02-21 15:11:39 -0800

    this file

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void){
+for(int i=97; i<=122; i++){
+putchar(i);
+}
+}
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
