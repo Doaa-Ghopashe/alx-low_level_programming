7b37e480e3b682e6362e64b15fcc6137d967d014
Doaa-Ghopashe <doaaadel252@yahoo.com>
2023-02-22 09:10:06 -0800

    this function is to check if the prompt value is lowercase or uppercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,19 +2,19 @@
 #include "main.h"
 
 /**
-* islower.Check if the character is in lowercase then return 1, otherwise return 0
+* islower.Check if the character is in lowercase
 *
 * Return: int
 */
 int _islower(int c)
 {
 int i;
-for(i = 97; i <= 122; i++)
+for (i = 97; i <= 122; i++)
 {
-if(c == i)
+if (c == i)
 {
-return 1;
+return (1);
 }
 }
-return 0;
+return (0);
 }
