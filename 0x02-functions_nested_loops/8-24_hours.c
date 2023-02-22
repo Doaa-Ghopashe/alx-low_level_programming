763d8344d8b748dc33e83d19edf559aaa93b83cc
Doaa-Ghopashe <doaaadel252@yahoo.com>
2023-02-22 11:27:32 -0800

    to print the minutes and seconds

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,33 +8,34 @@
   */
 void jack_bauer(void)
 {
-	int secs, mins = 0;
-	for (mins = 0; mins <= 23; mins++)
-	{
-	for (secs = 0; secs < 60; secs++)
-	{
-		if (mins < 10)
-		{
-			if (secs < 10)
-			{
-				printf("0%d:0%d\n", mins, secs);
-			}
-			else
-			{
-				printf("0%d:%d\n", mins, secs);
-			}
-		}
-		else
-		{
-			if (secs < 10)
-                        {
-                                printf("%d:0%d\n", mins, secs);
-                        }
-                        else
-                        {
-                                printf("%d:%d\n", mins, secs);
-                        }
-		}
-	}
-	}
+int secs, mins = 0;
+
+for (mins = 0; mins <= 23; mins++)
+{
+for (secs = 0; secs < 60; secs++)
+{
+if (mins < 10)
+{
+if (secs < 10)
+{
+printf("0%d:0%d\n", mins, secs);
+}
+else
+{
+printf("0%d:%d\n", mins, secs);
+}
+}
+else
+{
+if (secs < 10)
+{
+printf("%d:0%d\n", mins, secs);
+}
+else
+{
+printf("%d:%d\n", mins, secs);
+}
+}
+}
+}
 }
