2a2ee3251784c362df78002d425fb2069fb59bc5
Doaa-Ghopashe <doaaadel252@yahoo.com>
2023-02-22 09:51:36 -0800

    this function to check if the prompt value is a character or not

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * _isalpha - Checks for alphabetic character
+  * @c: The character to be checked
+  *
+  * Return: 1 for alphabetic character or 0 for anything else
+*/
+int _isalpha(int c){
+int i;
+for (i = 65; i <= 122; i++)
+{
+if (i == 91)
+{
+i = 97;
+}
+if (c == i)
+{
+return (1);
+}
+}
+return (0);
+}
