2151ac6931d79f0905434d384913b27e29ccfffe
Doaa-Ghopashe <doaaadel252@yahoo.com>
2023-02-22 07:57:08 -0800

    this function has been maintend

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,17 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
-  * print_alphabet - Make the alphabet
-  *
-  * Return: void
-  */
+* print_alphabet - Make the alphabet
+*
+* Return: void
+*/
 void print_alphabet(void)
 {
 int c;
 for (c = 97; c <= 122; c++)
 {
-putchar(c)
+putchar(c);
 }
+printf("\n");
 }
