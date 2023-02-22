ec2c5e6af48f048299c427b47237f94bce7e6c07
Doaa-Ghopashe <doaaadel252@yahoo.com>
2023-02-22 08:24:08 -0800

    the decelartion of variables used in the loop condition should be outside

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,17 +2,19 @@
 #include "main.h"
 
 /**
-* function to print the alphapet letters  times
+* print_alphabet_x10 - Make alphabet x10 times
 *
-* Return: Always 0.
+* Return: void
 */
 void print_alphabet_x10(void)
 {
-int i,c;
-for(i=0;i<10;i++){
-for(c=97; c<=122 ; c++){
+int i, c;
+for (i = 0; i < 10; i++)
+{
+for (c = 97; c <= 122; c++)
+
 putchar(c);
-}
+
 printf("\n");
 }
 }
