333c901edcedca5b50f547184235df8bd9401863
Doaa-Ghopashe <doaaadel252@yahoo.com>
2023-02-23 10:22:19 -0800

    this function to check if the input char is in the uppercase or lowercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,11 +2,11 @@
 #include "main.h"
 
 /**
- *issuper: this function to check if the uppercase
- *@c: this variable is the ASCII code for some variable
- *
- * Return: Always 0.
- */
+  * _isupper - Checks for lowercase character
+  * @c: The character to be checked
+  *
+  * Return: 1 for uppercase character or 0 for anything else
+  */
 int _isupper(int c)
 {
 int i;
